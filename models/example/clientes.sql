2d047ed96c89ac75eb3a3127c896a56a1d1a7ff6
Erick Mendonça <erickm.uff@gmail.com>
2024-02-25 05:44:56 +0000

    Salva a alteração em clientes.sql

--- models/example/clientes.sql
+++ models/example/clientes.sql
@@ -1,1 +1,2 @@
-SELECT *, FLOOR(DATE_DIFF('2024-02-21',data_nascimento, DAY)/365.25) AS idade FROM `banvic-415018.schema.clientes`
+SELECT *, FLOOR(DATE_DIFF('2024-02-21',data_nascimento, DAY)/365.25) AS idade 
+FROM `banvic-415018.schema.clientes`
